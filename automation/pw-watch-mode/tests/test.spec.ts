ffde5d4ddeedcd7becaaf5c4710b3e0d0c3fcc20
Sarah Woodhouse <sarah@joingelt.com>
2024-06-03 12:46:52 +0100

    Add console log to watched test

--- automation/pw-watch-mode/tests/test.spec.ts
+++ automation/pw-watch-mode/tests/test.spec.ts
@@ -3,6 +3,7 @@ import test from "@playwright/test";
 test.describe("My Test Suite", () => {
   test("Watch this test", async ({ page }) => {
     await test.step("Step 1", async () => {
+      console.log("Run the watched test");
       await page.goto("https://playwright.dev/");
       await page.hover("text=Get Started");
     });
